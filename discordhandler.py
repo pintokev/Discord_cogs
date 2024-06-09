e6ed3340faa0d89d8ebdd6ec4fa4eabf50cd0f52
pintok <pintok@machine.home>
2024-06-09 23:12:23 +0200

    modif pour supprimer le \n lors du code bloc

--- discordhandler.py
+++ discordhandler.py
@@ -66,7 +66,7 @@ def trouver_debut_bloc_code(message):
     pattern = r'```(\w+)'
     matches = re.findall(pattern, message)
     print(matches)
-    return len(matches), matches[-1]
+    return len(matches), "```"+matches[-1]
 def trouver_fin_bloc_code(message):
     pattern = r'```(?=\s|\n|$)'
     matches = re.findall(pattern, message)
