53030190e8d8ef7219c89788d466e0e7f7ffbd6b
Pintok <kevin.pinto.1995@gmail.com>
2026-03-19 01:40:52 +0100

    ajout des cogs nil, nim, nih

--- cogs/new_image_mid.py
+++ cogs/new_image_mid.py
@@ -12,7 +12,7 @@ class new_image(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="new_image", aliases=["nih"])
+    @commands.command(name="new_image", aliases=["nim"])
     async def new_image(self, ctx, *, message):
         '''Génère une image avec le dernier modèle de Gemini. Ne garde pas d'historique'''
         thread = await createThread(ctx, "Voici l'image")
